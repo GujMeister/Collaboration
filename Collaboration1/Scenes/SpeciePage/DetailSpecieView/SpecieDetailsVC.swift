a9d85414cbd7b5fbae1df2824722a08e6c6eb137
Me <lukagujejiani@gmail.com>
2024-05-18 00:39:57 +0400

    Added Scenes and Models

---
+++ Collaboration1/Scenes/SpeciePage/DetailSpecieView/SpecieDetailsVC.swift
@@ -0,0 +1,104 @@
+//
+//  SpecieDetailsVC.swift
+//  Collaboration1
+//
+//  Created by Luka Gujejiani on 17.05.24.
+//
+
+import UIKit
+
+final class SpecieDetailsVC: UIViewController {
+    // MARK: - Properties
+    internal let viewModel = SpecieDetailsVM()
+    private var dataSource: UICollectionViewDiffableDataSource<Int, NaturalistInfo.Taxon>!
+
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment -> NSCollectionLayoutSection? in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .estimated(300))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .estimated(300))
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            group.interItemSpacing = .fixed(10)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 10
+            section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            return section
+        }
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(SpecieCollectionViewCell.self, forCellWithReuseIdentifier: SpecieCollectionViewCell.identifier)
+        collectionView.alwaysBounceVertical = true
+        collectionView.backgroundColor = UIColor(hex: "262A34")
+        return collectionView
+    }()
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupDataSource()
+        setupBindings()
+    }
+    
+    // MARK: - Setup UI
+    private func setupUI() {
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - Bind ViewModel
+    private func setupBindings() {
+        viewModel.onSpeciesInfoUpdate = { [weak self] species in
+            self?.updateSnapshot(with: species)
+        }
+    }
+}
+
+
+extension SpecieDetailsVC: UICollectionViewDelegate {
+    // MARK: - Setup Data Source
+    private func setupDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Int, NaturalistInfo.Taxon>(collectionView: collectionView) { collectionView, indexPath, taxon in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SpecieCollectionViewCell.identifier, for: indexPath) as? SpecieCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.configure(with: taxon)
+            cell.setWikipediaAction {
+                if let urlString = taxon.wikipediaUrl, let url = URL(string: urlString) {
+                    UIApplication.shared.open(url)
+                }
+            }
+            return cell
+        }
+    }
+    
+    // MARK: - Update Snapshot
+    private func updateSnapshot(with species: [NaturalistInfo.Taxon]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, NaturalistInfo.Taxon>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(species)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+
+    @objc private func openWikipedia(_ sender: UIButton) {
+        if let indexPath = collectionView.indexPath(for: sender.superview?.superview as! SpecieCollectionViewCell) {
+            let taxon = dataSource.itemIdentifier(for: indexPath)
+            if let urlString = taxon?.wikipediaUrl, let url = URL(string: urlString) {
+                UIApplication.shared.open(url)
+            }
+        }
+    }
+}
---
+++ Collaboration1/Scenes/SpeciePage/SpeciePageView/SpeciePageVC.swift
@@ -0,0 +1,310 @@
+//
+//  SpeciePageVC.swift
+//  Collaboration1
+//
+//  Created by Luka Gujejiani on 17.05.24.
+//
+
+import UIKit
+
+final class SpeciePageVC: UIViewController {
+    // MARK: - Properties
+    private let viewModel = SpeciePageVM()
+    private var isSearching = false
+
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(CountryCollectionViewCell.self, forCellWithReuseIdentifier: CountryCollectionViewCell.reuseIdentifier)
+        return collectionView
+    }()
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.delegate = self
+        searchBar.placeholder = "Search Countries"
+        searchBar.barTintColor = UIColor(hex: "#262A34")
+        searchBar.searchTextField.textColor = .white
+        
+        if let searchTextField = searchBar.value(forKey: "searchField") as? UITextField {
+            let placeholderText = searchBar.placeholder ?? ""
+            searchTextField.attributedPlaceholder = NSAttributedString(
+                string: placeholderText,
+                attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+            )
+            
+            if let leftView = searchTextField.leftView as? UIImageView {
+                leftView.tintColor = .white
+            }
+        }
+        return searchBar
+    }()
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        bindViewModel()
+        viewModel.filteredCountryCapitals = countryFlagsAndCapitals
+    }
+    
+    // MARK: - Setup UI
+    private func setupUI() {
+        view.backgroundColor = UIColor(hex: "#3C4251")
+        
+        view.addSubview(searchBar)
+        view.addSubview(collectionView)
+        
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - Bind ViewModel
+    private func bindViewModel() {
+        viewModel.onFilteredCountriesUpdate = { [weak self] in
+            DispatchQueue.main.async {
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension SpeciePageVC: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        isSearching = !searchText.isEmpty
+        viewModel.filterCountries(with: searchText)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        isSearching = false
+        viewModel.filterCountries(with: "")
+    }
+}
+
+// MARK: - Data Source
+extension SpeciePageVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.getCountriesCount()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CountryCollectionViewCell.reuseIdentifier, for: indexPath) as? CountryCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let (country, (flagEmoji, capital)) = viewModel.getCountry(at: indexPath.row)
+        
+        cell.configureCell(with: flagEmoji, text1: country, text2: capital)
+        
+        cell.backgroundColor = UIColor(hex: "262A34")
+        cell.layer.cornerRadius = 10
+        
+        return cell
+    }
+}
+
+// MARK: - Delegate Flow Layout
+extension SpeciePageVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfItemsPerRow: CGFloat = 3
+        let spacingBetweenItems: CGFloat = 5
+        
+        let totalSpacing = (2 * spacingBetweenItems) + ((numberOfItemsPerRow - 1) * spacingBetweenItems)
+        let width = (collectionView.bounds.width - totalSpacing) / numberOfItemsPerRow
+        
+        return CGSize(width: width, height: 160)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+}
+
+// MARK: - Delegate
+extension SpeciePageVC: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let (selectedCountry, _) = viewModel.getCountry(at: indexPath.row)
+        navigateToSpecieDetails(with: selectedCountry)
+    }
+    
+    func navigateToSpecieDetails(with country: String) {
+        let specieDetailsVC = SpecieDetailsVC()
+        specieDetailsVC.viewModel.fetchCityID(with: country)
+        self.present(specieDetailsVC, animated: true)
+    }
+}
+
+
+
+//
+//final class SpeciePageVC: UIViewController, UISearchBarDelegate {
+//    // MARK: - Properties
+//    private let viewModel = SpeciePageVM()
+//    
+//    lazy var collectionView: UICollectionView = {
+//        let layout = UICollectionViewFlowLayout()
+//        layout.scrollDirection = .vertical
+//        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//        collectionView.backgroundColor = .clear
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
+//        collectionView.register(CountryCollectionViewCell.self, forCellWithReuseIdentifier: CountryCollectionViewCell.reuseIdentifier)
+//        return collectionView
+//    }()
+//    
+//    lazy var searchBar: UISearchBar = {
+//        let searchBar = UISearchBar()
+//        searchBar.delegate = self
+//        searchBar.placeholder = "Search Countries"
+//        searchBar.barTintColor = UIColor(hex: "#262A34")
+//        searchBar.searchTextField.textColor = .white
+//        
+//        if let searchTextField = searchBar.value(forKey: "searchField") as? UITextField {
+//            let placeholderText = searchBar.placeholder ?? ""
+//            searchTextField.attributedPlaceholder = NSAttributedString(
+//                string: placeholderText,
+//                attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+//            )
+//            
+//            if let leftView = searchTextField.leftView as? UIImageView {
+//                leftView.tintColor = .white
+//            }
+//        }
+//        return searchBar
+//    }()
+//    
+//    // MARK: - LifeCycle
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setupUI()
+//        bindViewModel()
+//        viewModel.filteredCountryCapitals = countryFlagsAndCapitals
+//    }
+//    
+//    // MARK: - Setup UI
+//    func setupUI() {
+//        view.backgroundColor = UIColor(hex: "#3C4251")
+//        
+//        view.addSubview(searchBar)
+//        view.addSubview(collectionView)
+//        
+//        searchBar.translatesAutoresizingMaskIntoConstraints = false
+//        collectionView.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        NSLayoutConstraint.activate([
+//            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            
+//            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+//            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+//            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
+//            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+//        ])
+//    }
+//    
+//    // MARK: - Bind ViewModel
+//    private func bindViewModel() {
+//        viewModel.onFilteredCountriesUpdate = { [weak self] in
+//            DispatchQueue.main.async {
+//                self?.collectionView.reloadData()
+//            }
+//        }
+//    }
+//    
+//    // MARK: - UISearchBarDelegate
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        viewModel.setSearchText(searchText)
+//    }
+//    
+//    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+//        searchBar.text = ""
+//        viewModel.setSearchText("")
+//    }
+//}
+//
+//// MARK: - Data Source
+//extension SpeciePageVC: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return viewModel.getCountriesCount()
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CountryCollectionViewCell.reuseIdentifier, for: indexPath) as? CountryCollectionViewCell else {
+//            return UICollectionViewCell()
+//        }
+//        
+//        let (country, (flagEmoji, capital)) = viewModel.getCountry(at: indexPath.row)
+//        
+//        cell.configureCell(with: flagEmoji, text1: country, text2: capital)
+//        
+//        cell.backgroundColor = UIColor(hex: "262A34")
+//        cell.layer.cornerRadius = 10
+//        
+//        return cell
+//    }
+//}
+//
+//// MARK: - Delegate Flow Layout
+//extension SpeciePageVC: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let numberOfItemsPerRow: CGFloat = 3
+//        let spacingBetweenItems: CGFloat = 5
+//        
+//        let totalSpacing = (2 * spacingBetweenItems) + ((numberOfItemsPerRow - 1) * spacingBetweenItems)
+//        let width = (collectionView.bounds.width - totalSpacing) / numberOfItemsPerRow
+//        
+//        return CGSize(width: width, height: 160)
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 5
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 5
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+//    }
+//}
+//
+//// MARK: - Delegate
+//extension SpeciePageVC: UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let (selectedCountry, _) = viewModel.getCountry(at: indexPath.row)
+//        navigateToSpecieDetails(with: selectedCountry)
+//    }
+//    
+//    func navigateToSpecieDetails(with country: String) {
+//        let specieDetailsVC = SpecieDetailsVC()
+//        specieDetailsVC.viewModel.fetchCityID(with: country)
+//        self.present(specieDetailsVC, animated: true)
+//    }
+//}
