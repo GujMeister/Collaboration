d8b862eaa5bafb8fcedca45dfc2529c904a5d71a
Nodiko Gatchava <ghachava.nodar@tbcacademy.edu.ge>
2024-05-18 15:51:08 +0400

    Added MVVM Model

--- Collaboration1/Scenes/WeatherPage/WeatherPageVC.swift
+++ Collaboration1/Scenes/WeatherPage/WeatherPageVC.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class WeatherPageVC: UIViewController {
     // MARK: - Variables
-    var countryText = "Kutaisi"
-    let urlString = "http://api.openweathermap.org/data/2.5/forecast?q=kutaisi&appid=159e264bbb707514e8ea1734c14e4169"
+    
+    let viewModel = WeatherPageVM()
     
     // MARK: - UI Components
     private let background: UIImageView = {
@@ -32,25 +32,91 @@ class WeatherPageVC: UIViewController {
         return textField
     }()
     
+    private let countryLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.text = "country Name"
+        return label
+    }()
    
+    private let tempLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.text = "temp"
+        return label
+    }()
     
+    private let weatherLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.text = "weather"
+        return label
+    }()
     
+    private let maxTempLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.text = "max"
+        return label
+    }()
+    
+    private let minTempLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.text = "min"
+        return label
+    }()
+    
+    private let timezoneLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.text = "timezone"
+        return label
+    }()
+    
+    private let searchButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Search", for: .normal)
+        button.backgroundColor = UIColor(named: "weatherCol")
+        button.layer.cornerRadius = 2
+        button.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+        return button
+    }()
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        
+        viewModel.onDataFetched = { [weak self] weather in
+            guard let self = self, let weather = weather else { return }
+            DispatchQueue.main.async {
+                self.updateUI(with: weather)
+            }
+        }
+        
     }
     
     
     // MARK: - UI Setup
     func setupUI() {
         self.view.backgroundColor = .red
-        let views = [background, countryName]
+        let views = [background, countryName, countryLabel, tempLabel, weatherLabel, maxTempLabel, minTempLabel, timezoneLabel, searchButton]
         views.forEach { view in
             self.view.addSubview(view)
             view.translatesAutoresizingMaskIntoConstraints = false
         }
-        countryText = countryName.text!
         
         NSLayoutConstraint.activate([
             countryName.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
@@ -62,8 +128,45 @@ class WeatherPageVC: UIViewController {
             background.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
             background.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             background.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            
+            countryLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 250),
+            countryLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            tempLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 290),
+            tempLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            weatherLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 330),
+            weatherLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+             
+            maxTempLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 370),
+            maxTempLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            minTempLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 410),
+            minTempLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            timezoneLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 450),
+            timezoneLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            searchButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            searchButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 500),
+            searchButton.heightAnchor.constraint(equalToConstant: 40),
+            searchButton.widthAnchor.constraint(equalToConstant: 200),
         ])
     }
-    
+    @objc func searchButtonTapped() {
+        guard let city = countryName.text, !city.isEmpty else {
+            print("Please enter a city name.")
+            return
+        }
+        viewModel.fetchWeather(with: city)
+    }
+    func updateUI(with weather: WeatherForecast) {
+        if let temp = weather.list.first?.main.temp {
+            tempLabel.text = "\(temp-273.15)°C"
+        }
+        if let minTemp = weather.list.first?.main.temp_min {
+            minTempLabel.text = "\(minTemp-273.13)"
+        }
+    }
 }
 
