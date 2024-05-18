f8f1388263c9473b55e4426140485f94f19a34f1
Nodiko Gatchava <ghachava.nodar@tbcacademy.edu.ge>
2024-05-18 22:08:08 +0400

    Final version

--- Collaboration1/Scenes/WeatherPage/WeatherPageVC.swift
+++ Collaboration1/Scenes/WeatherPage/WeatherPageVC.swift
@@ -13,23 +13,48 @@ class WeatherPageVC: UIViewController {
     let viewModel = WeatherPageVM()
     
     // MARK: - UI Components
-        
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 25, weight: .semibold)
+        label.font = UIFont(name: "FiraGO-Bold", size: 22)
+        label.text = "Weather Forecast"
+        return label
+    }()
+    private let inputLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.font = UIFont(name: "FiraGO-Bold", size: 13)
+        label.text = "Enter City Name"
+        return label
+    }()
     private let countryName: UITextField = {
+        var placeHolderColor = UIColor.white
+        placeHolderColor = UIColor(white: 1, alpha: 0.5)
         let textField = UITextField()
-        textField.placeholder = "Enter Country Name"
+        textField.placeholder = "Example: Kutaisi"
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "Example: Kutaisi",
+            attributes: [NSAttributedString.Key.foregroundColor: placeHolderColor]
+        )
+        textField.textAlignment = .natural
         textField.layer.cornerRadius = 2
         textField.layer.borderWidth = 2
         textField.layer.borderColor = UIColor(hex: "262A34").cgColor
         textField.textColor = .white
-        textField.font = UIFont(name: "FiraGO", size: 12)
+        textField.font = UIFont(name: "FiraGO-Bold", size: 12)
         return textField
     }()
     
-    private let countryLabel: UILabel = {
+    private let cityLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
         label.textAlignment = .left
-        label.font = .systemFont(ofSize: 25, weight: .semibold)
+        label.font = .systemFont(ofSize: 25, weight: .bold)
+        label.font = UIFont(name: "FiraGO-Bold", size: 25)
         return label
     }()
    
@@ -38,6 +63,7 @@ class WeatherPageVC: UIViewController {
         label.textColor = .white
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 60, weight: .semibold)
+        label.font = UIFont(name: "FiraGO-Bold", size: 60)
         return label
     }()
     
@@ -45,7 +71,8 @@ class WeatherPageVC: UIViewController {
         let label = UILabel()
         label.textColor = .white
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.font = UIFont(name: "FiraGO-Bold", size: 22)
         return label
     }()
     
@@ -53,7 +80,8 @@ class WeatherPageVC: UIViewController {
         let label = UILabel()
         label.textColor = .white
         label.textAlignment = .left
-        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.font = .systemFont(ofSize: 22, weight: .regular)
+        label.font = UIFont(name: "FiraGO-Bold", size: 22)
         return label
     }()
     
@@ -61,7 +89,8 @@ class WeatherPageVC: UIViewController {
         let label = UILabel()
         label.textColor = .white
         label.textAlignment = .left
-        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.font = .systemFont(ofSize: 22, weight: .regular)
+        label.font = UIFont(name: "FiraGO-Bold", size: 22)
         return label
     }()
     
@@ -70,16 +99,17 @@ class WeatherPageVC: UIViewController {
         let button = UIButton()
         button.setTitle("Search", for: .normal)
         button.backgroundColor = UIColor(hex: "262A34")
-        button.layer.cornerRadius = 2
-        button.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+        button.layer.cornerRadius = 10
+//        button.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
         return button
     }()
     
     private  let weatherImage: UIImageView = {
+        let colours = [UIColor.red, UIColor.yellow, UIColor.red]
+        let configuration = UIImage.SymbolConfiguration(paletteColors: colours)
         let image = UIImageView()
         image.contentMode = .scaleAspectFit
-        image.image = UIImage(systemName: "thermometer.sun")?.withTintColor(.red, renderingMode: .alwaysOriginal)
-        image.tintColor = .white
+        image.image = UIImage(systemName: "thermometer.sun")?.applyingSymbolConfiguration(configuration)
         return image
     }()
     // MARK: - LifeCycle
@@ -100,20 +130,28 @@ class WeatherPageVC: UIViewController {
     // MARK: - UI Setup
     func setupUI() {
         self.view.backgroundColor = UIColor(hex: "#3C4251")
-        let views = [ countryName, countryLabel, tempLabel, weatherLabel, maxTempLabel, minTempLabel, searchButton, weatherImage]
+        let views = [ countryName, cityLabel, tempLabel, weatherLabel, maxTempLabel, minTempLabel, searchButton, weatherImage, titleLabel, inputLabel]
         views.forEach { view in
             self.view.addSubview(view)
             view.translatesAutoresizingMaskIntoConstraints = false
         }
-        
+        searchButton.addAction(UIAction(handler: { _ in
+            self.searchButtonTapped()
+        }), for: .touchUpInside)
         NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 53),
+            titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            inputLabel.bottomAnchor.constraint(equalTo: countryName.topAnchor, constant: -5),
+            inputLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 24),
+            
             countryName.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             countryName.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -250),
             countryName.widthAnchor.constraint(equalToConstant: 350),
             countryName.heightAnchor.constraint(equalToConstant: 40),
             
-            countryLabel.bottomAnchor.constraint(equalTo: tempLabel.topAnchor, constant: 10),
-            countryLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            cityLabel.bottomAnchor.constraint(equalTo: tempLabel.topAnchor, constant: -3),
+            cityLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             
             tempLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 290),
             tempLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
@@ -138,9 +176,12 @@ class WeatherPageVC: UIViewController {
             weatherImage.heightAnchor.constraint(equalToConstant: 150),
         ])
     }
-    @objc func searchButtonTapped() {
+    func searchButtonTapped() {
         guard let city = countryName.text, !city.isEmpty else {
             print("Please enter a city name.")
+            let alert = UIAlertController(title: "Error", message: "Please Fill the Field Correctly", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
             return
         }
         viewModel.fetchWeather(with: city)
@@ -159,16 +200,18 @@ class WeatherPageVC: UIViewController {
         if let weatherToday = weather.list.first?.weather.first?.main {
             weatherLabel.text = weatherToday
             if weatherToday == "Clouds" {
-                weatherImage.image = UIImage(systemName: "cloud.fill")
+                weatherImage.image = UIImage(systemName: "cloud.fill")?.withTintColor(.white, renderingMode: .alwaysOriginal)
             } else if weatherToday == "Clear" {
                 weatherImage.image = UIImage(systemName: "sun.max")?.withTintColor(.yellow, renderingMode: .alwaysOriginal)
             } else if weatherToday == "Rain" {
-                weatherImage.image = UIImage(systemName: "cloud.rain")
+                let colours = [UIColor.white, UIColor.blue]
+                let configuration = UIImage.SymbolConfiguration(paletteColors: colours)
+                weatherImage.image = UIImage(systemName: "cloud.rain")?.applyingSymbolConfiguration(configuration)
             } else if weatherToday == "Wind" {
-                weatherImage.image = UIImage(systemName: "wind")
+                weatherImage.image = UIImage(systemName: "wind")?.withTintColor(.white, renderingMode: .alwaysOriginal)
             }
         }
-        countryLabel.text = countryName.text
+        cityLabel.text = countryName.text
     }
 }
 
